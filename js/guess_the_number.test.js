bb2159527cee02289ee3e0f0eeaa4305f4168bb5
ngocanh0202 <89017204+ngocanh0202@users.noreply.github.com>
2023-01-02 23:17:00 +0700

    up

--- js/guess_the_number.test.js
+++ js/guess_the_number.test.js
@@ -1,8 +1,12 @@
 
 const point = require('./guess_the_number')
 
-test('make sure function is right',()=>{
+test('Check score increases with correct answer',()=>{
     expect(point(2,2,4,2)).toEqual(3)
-    expect(point(3,3,9,0)).toEqual(0)
+})
+test('Check score drop when wrong answer',()=>{
     expect(point(2,3,6,2)).toEqual(1)
+})
+test('Scores of 0 will not decrease anymore',()=>{
+    expect(point(3,3,9,0)).toEqual(0)
 })
